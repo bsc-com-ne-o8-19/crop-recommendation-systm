c1d5cff7634a7ab642de84e92d6ed934674d7880
GEOFFREY MAKUMBA <BSC/COM/NE/20/19@COMLAB.COM>
2024-03-31 19:18:30 +0200

    website

---
+++ project/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    about_text = """Our system integrates advanced soil analysis techniques with sophisticated crop recommendation algorithms to provide tailored guidance to farmers. By analyzing soil composition, nutrient levels, and environmental factors, we deliver personalized recommendations for crop selection, fertilization, irrigation, and pest management."""
+    return about_text
+
+
+@app.route('/contact')
+def contact():
+    contact_info = {
+        'telephone': '+265881401065',
+        'email': 'mufa@gmail.com'
+    }
+    return render_template('contact.html', contact_info=contact_info)
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        return redirect(url_for('home')) 
+    return render_template('predict.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
