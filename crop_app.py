0b7688cc82cdcc7b62ca7bd98d1fd7d964af4954
bsc-com-ne-11-19 <bsc-com-ne-11-19@unima.ac.mw>
2024-04-16 23:47:46 +0200

    adding files that had been skipped

--- crop_app.py
+++ crop_app.py
@@ -25,6 +25,18 @@ def predict_crops_with_alternatives(input_features, n_alternatives=2):
 @app.route('/')
 def home():
     return render_template('Home.html')
+@app.route('/about')
+def about():
+    about_text = """Our system integrates advanced soil analysis techniques with sophisticated crop recommendation algorithms to provide tailored guidance to farmers. By analyzing soil composition, nutrient levels, and environmental factors, we deliver personalized recommendations for crop selection, fertilization, irrigation, and pest management."""
+    return about_text
+
+@app.route('/Help')
+def help():
+    contact_info = {
+        'telephone': '+265881401065',
+        'email': 'croprecommendation@gmail.com'
+    }
+    return render_template('Help.html', contact_info=contact_info)
 
 @app.route('/predict')
 def prediction():
